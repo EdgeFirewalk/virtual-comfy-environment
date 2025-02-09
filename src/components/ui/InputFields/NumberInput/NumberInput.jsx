79d7884086b9590d205e444a82baa327d139a3d2
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2025-02-09 15:28:37 +0600

    Сделал поля ввода по умолчанию валидными

--- src/components/ui/InputFields/NumberInput/NumberInput.jsx
+++ src/components/ui/InputFields/NumberInput/NumberInput.jsx
@@ -3,7 +3,7 @@ import styles from '../InputField.module.css';
 const NumberInput = ({ className, isValid, ...props }) => {
   return (
     <input
-      className={`${styles.inputField} ${!isValid && styles.invalid} ${className}`}
+      className={`${styles.inputField} ${isValid === false ? styles.invalid : ''} ${className}`}
       type="number"
       {...props}
     />
