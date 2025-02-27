1f11cca6c9654d50fd3c92c02efbe0a4186936ca
AnnaTupota <annatupotaalexxandrovna@mail.ru>
2025-02-27 20:45:03 +0600

    Поставила свои импорты на место комментариев и в том порядке, в котором используем ниже компоненты, убрала кнопку Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
 // import Pomodoro
 import Sounds from './components/layout/Sounds/Sounds';
-// import Radio
-import ManualAndFullscreenButton from './components/layout/ManualAndFullscreenButton/ManualAndFullscreenButton';
-//import Button
-import Button from './components/ui/Button/Button'; 
+
 import Radio from './components/layout/Radio/Radio';
+
+import ManualAndFullscreenButton from './components/layout/ManualAndFullscreenButton/ManualAndFullscreenButton';
 
 function App() {
 
-  
-  const handleClick = () => {
-    console.log('Кнопка нажата!');
-  };
   return (
     <>
-    {/* Radio */}
-    <Radio />
-    
-    {/* Button */}
-    <div className="App">
-      <Button onClick={handleClick}>Add Station</Button>
-    </div>
-
       {/* Pomodoro */}
       <Sounds />
-      {/* Radio */}
+      <Radio />
       <ManualAndFullscreenButton />
     </>
   );
