006db23ce926bee9cceae65df73bbcbf0b8c46f0
AnnaTupota <annatupotaalexxandrovna@mail.ru>
2025-02-27 20:50:30 +0600

    Сделала достойный переиспользуемый компонент, использовала переменные  variables.css, убрала переменные из своего файла css

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import styles from './Button.module.css'; 
+import styles from './Button.module.css';
 
-const Button = ({ children, onClick }) => {
+const Button = ({ className, icon, text, children, ...props }) => {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={`${styles.button} ${className}`} {...props}>
+      {/* Иконка, если она передана */}
+      {icon && <span className={styles.icon}>{icon}</span>}
+
+      {/* Текст, если он передан */}
+      {text && <span className={styles.text}>{text}</span>}
+
+      {/* Дочерние элементы, если они переданы */}
       {children}
     </button>
   );
