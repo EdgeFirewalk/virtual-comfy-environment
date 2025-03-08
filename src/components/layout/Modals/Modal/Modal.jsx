a2d2b80b474388f72afa1f57ef7981e7d33f21ee
AnnaTupota <annatupotaalexxandrovna@mail.ru>
2025-03-08 21:14:25 +0600

    Видимость модального окна управляется с помощью классов(реализована базовая логика закрытия модального окна

--- src/components/layout/Modals/Modal/Modal.jsx
+++ src/components/layout/Modals/Modal/Modal.jsx
@@ -1,40 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+
 import styles from './Modal.module.css';
+
 import { FiX } from 'react-icons/fi';
+
 import SquareButton from '../../../ui/SquareButton/SquareButton';
 
 const Modal = ({ isOpen, onClose, label, children }) => {
-  const [isClosing, setIsClosing] = useState(false);
-
-  // Эффект для обработки анимации закрытия
-  useEffect(() => {
-    if (!isOpen) {
-      setIsClosing(false); // Сбрасываем состояние закрытия
-    }
-  }, [isOpen]);
-
-  // Функция для обработки закрытия с анимацией
-  const handleClose = () => {
-    setIsClosing(true); // Запускаем анимацию закрытия
-    setTimeout(() => {
-      onClose(); // Закрываем модальное окно после завершения анимации
-    }, 500); // Время анимации (0.5 секунды)
-  };
-
-  if (!isOpen && !isClosing) return null;
-
   return (
     <div
-      className={`${styles.modalOverlay} ${isClosing ? styles.closing : ''}`}
-      onClick={handleClose}
+      className={`${styles.modalOverlay} ${isOpen ? styles.open : styles.closed}`}
+      onClick={onClose}
     >
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         {/* Заголовок и кнопка закрытия */}
-        <div className={styles.modal_header}>
+        <div className={styles.modalHeader}>
           {label && <div className={styles.label}>{label}</div>}
           <SquareButton
             icon={<FiX />}
-            onClick={handleClose}
+            onClick={onClose}
             className={styles.closeButton}
           />
         </div>
