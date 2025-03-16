58a8626bc332fd050a2b50830a3eb8f8278b870a
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2025-03-16 15:15:16 +0600

    Добавил и написал страницу Sounds, добавил заготовку страницы Radio

---
+++ src/components/layout/Manual/ManualPages/RadioPage/RadioPage.jsx
@@ -0,0 +1,11 @@
+import typography from '../Typography.module.css';
+
+const RadioPage = () => {
+  return (
+    <>
+      <h1 className={typography.title}>Radio</h1>
+    </>
+  );
+};
+
+export default RadioPage;
